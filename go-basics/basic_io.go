de5cd61c79e0d5c6c9e361002fabe91db50b32c8
codeworx <mailtocode@gmail.com>
2026-01-12 20:39:43 -0600

    complex demo

--- go-basics/basic_io.go
+++ go-basics/basic_io.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
 
 func main() {
-	fmt.Println("This is the first print that demonstrates go fmt")
+	fmt.Printf("\n========================\n")
+	fmt.Printf("\nHere is a list of Demos:\n")
+	fmt.Printf("\n========================\n")
+	demoComplex()
+}
+
+func demoComplex() {
+	fmt.Printf("\nDemo of complex numbers: \n")
+	first := complex(4.2, 3.3)
+	second := complex(5.3, 2.2)
+
+	sum := first + second
+	prod := first * second
+	div := first / second
+	diff := first - second
+	real := real(first)
+	img := imag(second)
+	absVal := cmplx.Abs(first)
+
+	fmt.Printf("\nSum: %4.2f\nProduct: %4.2f\nDivision: %4.2f"+
+		"\nDifference: %4.2f\nReal: %4.2f\nImaginary: %4.2f"+
+		"\nAbsolute Value: %4.2f\n", sum, prod, div, diff, real, img, absVal)
 }
