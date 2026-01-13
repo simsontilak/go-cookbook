5448e2459e0f2851d527cc7ad489e86025aa9a5c
codeworx <mailtocode@gmail.com>
2026-01-12 21:50:28 -0600

    type conversion

--- go-basics/basic_io.go
+++ go-basics/basic_io.go
@@ -10,6 +10,21 @@ func main() {
 	fmt.Printf("\nHere is a list of Demos:\n")
 	fmt.Printf("\n========================\n")
 	demoComplex()
+	demoTypeConversion()
+}
+
+func demoTypeConversion() {
+	fmt.Printf("\nDemo of type conversion: \n")
+	var x int = 10
+	var y float64 = 44.5324
+	var sum1 float64 = float64(x) + y
+	var sum2 int = x + int(y)
+	fmt.Println("Sum1 = ", sum1, "Sum2 = ", sum2)
+	var a int = 10
+	var b byte = 20
+	var sum3 int = a + int(b)
+	var sum4 byte = byte(a) + b
+	fmt.Println("Sum3 = ", sum3, "Sum4 = ", sum4)
 }
 
 func demoComplex() {
